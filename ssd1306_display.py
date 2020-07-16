a20f350404e676171ac69d384d71b00c2187489c
Greg Smith <gregory_smith@gregoftheweb.com>
2020-07-16 12:20:10 -0400

    Cleanup and changes

--- ssd1306_display.py
+++ ssd1306_display.py
@@ -1,66 +1,80 @@
+import subprocess
+import math
+import asyncio
+import time
+
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
 from RPi import GPIO
-import asyncio
-import time 
+
 
 def round_down(n, decimals=0):
     multiplier = 10 ** decimals
     return math.floor(n * multiplier) / multiplier
 
+
+def get_shell_output(cmd: str):
+    return subprocess.check_output(cmd, shell=True)
+
+
 class InfoButton:
-    def __init__(self, info_btn_pin = 20, display_duration = 5, hold_time = 1):
+    def __init__(self, info_btn_pin=20, display_duration=5, hold_time=1, time_to_restart=3, time_to_shutdown=3,
+                 time_to_cancel=3):
         serial = i2c(port=1, address=0x3c)
         self.device = ssd1306(serial, height=32, rotate=2)
-        
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(info_btn_pin, GPIO.IN)
-        
-        self.wait_timer = None
+
         self.display_duration = display_duration
         self.pin = info_btn_pin
+        self.hold_time = hold_time
+        self.time_to_restart = time_to_restart
+        self.time_to_shutdown = time_to_shutdown + time_to_restart
+        self.time_to_cancel = time_to_cancel + time_to_shutdown + time_to_restart
+
+        self.wait_timer = None
         self.pressed = False
         self.held = False
         self.press_display = False
         self.hold_display = False
         self.release_display = False
-        self.hold_start_time = 0
-        self.press_time = 0
-        self.hold_time = hold_time
         self.display_task = None
         self.pending_task = None
+        self.hold_start_time = 0
+        self.press_time = 0
         self.presses = 0
-        
+
         asyncio.run(self._run())
-        
+
     async def _run(self):
         await asyncio.gather(self._monitor_input(), self._monitor_display())
-        
+
     async def _monitor_input(self):
         while True:
             if GPIO.input(self.pin) == 0:
-                if self.pressed == False:
+                if not self.pressed:
                     self.pressed = True
-                    self.on_press()
+                    self._on_press()
                     self.press_time = time.time()
-                elif self.held == False and time.time() - self.press_time > self.hold_time:
+                elif not self.held and time.time() - self.press_time > self.hold_time:
                     self.held = True
                     self.hold_start_time = self.press_time
-                elif self.held == True:
+                elif self.held:
                     held_time = round(time.time() - self.hold_start_time) + self.hold_time
-                    self.on_hold(held_time)
-                
-            elif GPIO.input(self.pin) == 1 and self.pressed == True and self.held == False:
+                    self._on_hold(held_time)
+
+            elif GPIO.input(self.pin) == 1 and self.pressed and not self.held:
                 self.pressed = False
-                self.on_short_release()
-            elif GPIO.input(self.pin) == 1 and self.held == True:
+                self._on_short_release()
+            elif GPIO.input(self.pin) == 1 and self.held:
                 self.pressed = False
                 self.held = False
-                held_time = round(time.time() - self.hold_start_time) + self.hold_time 
-                self.on_long_release(held_time)
+                held_time = round(time.time() - self.hold_start_time) + self.hold_time
+                self._on_long_release(held_time)
             await asyncio.sleep(0.001)
-                
+
     async def _monitor_display(self):
         while True:
             self._update_task()
@@ -70,75 +84,103 @@ class InfoButton:
                 except asyncio.CancelledError:
                     pass
                 else:
-                    self.reset()
+                    self._reset()
             await asyncio.sleep(0.001)
-        
+
+    @property()
+    def cpu(self):
+        return get_shell_output("top -bn1 | grep load | awk '{printf \"CPU: %3d%%\", $(NF-2)*100/4}'")
+
+    @property()
+    def memory(self):
+        return get_shell_output("free -m | awk 'NR==2{printf \"MEM: %3d%%\", $3*100/$2 }'")
+
+    @property()
+    def hostname(self):
+        return get_shell_output("hostname")
+
+    @property()
+    def ip_address(self):
+        return get_shell_output("hostname -I | cut -d\' \' -f1")
+
+    @property()
+    def uptime(self):
+        return get_shell_output("uptime -p")
+
     def _update_task(self):
         if (self.display_task is None or self.display_task.cancelled) and self.pending_task is not None:
             self.display_task = self.pending_task
             self.pending_task = None
-    
-    def reset_tasks(self):
+
+    def _reset_tasks(self):
         self.display_task = None
         self.pending_task = None
-                
-    def on_hold(self, held_time):
+
+    def _on_hold(self, held_time):
+        dots = ". " * (held_time % 3)
         self.hold_display = True
         self.release_display = False
         self.press_display = False
         self.presses = 0
-        self.set_display("Holding for {}s".format(held_time))
-    
-    def on_press(self):
+        if held_time >= self.time_to_cancel:
+            self._display_msg(middle_row="Release to cancel" + dots)
+        elif held_time >= self.time_to_shutdown:
+            self._display_msg(middle_row="Release to shutdown" + dots)
+        elif held_time >= self.time_to_restart:
+            self._display_msg(middle_row="Release to restart" + dots)
+        else:
+            self._display_msg(middle_row=dots)
+
+    def _on_press(self):
         self.hold_display = False
         self.release_display = False
         self.press_display = True
-        
-        
-    def on_short_release(self):
-        self.set_display_delayed("Pressed {} times".format(self.presses), self.display_duration)
-        if self.press_display:
-             self.presses = (self.presses + 1) % 3
-        
-        
-    def on_long_release(self, held_time):
+
+    def _on_short_release(self):
+        self._display_msg(top_row=self.hostname, middle_row=self.ip_address,
+                          bottom_row="CPU: %s | MEM: %S".format(self.cpu, self.memory))
+
+    def _on_long_release(self, held_time):
         self.hold_display = False
         self.release_display = True
         self.press_display = False
-        self.set_display_delayed("Released after {}s".format(held_time), self.display_duration)
-        
-    def display_msg(self, msg: str):
+        if held_time >= self.time_to_cancel:
+            self._display_msg(middle_row="Cancelling shutdown...")
+        elif held_time >= self.time_to_shutdown:
+            self._display_msg(middle_row="Shutting down...")
+        elif held_time >= self.time_to_restart:
+            self._display_msg(middle_row="Restarting...")
+        else:
+            self._display_msg(top_row=self.hostname, middle_row=self.ip_address,
+                              bottom_row="CPU: %s | MEM: %S".format(self.cpu, self.memory))
+        self._set_delay(self.display_duration)
+
+    def _display_msg(self, top_row: str = '', middle_row: str = '', bottom_row: str = ''):
+        if self.display_task is not None:
+            self.display_task.cancel()
+        self._reset_tasks()
         with canvas(self.device) as draw:
-            draw.text((5,1), msg, fill="white")
-            
-    def clear_screen(self):
+            draw.text((0, 0), top_row, fill="white")
+            draw.text((0, 12), middle_row, fill="white")
+            draw.text((0, 24), bottom_row, fill="white")
+
+    def _clear_screen(self):
         with canvas(self.device) as draw:
             draw.rectangle(self.device.bounding_box, fill="black")
-            
-    def reset_timer(self):
+
+    def _reset_timer(self):
         self.display_timer = 0
-        
-    def set_display(self, msg: str):
-        if self.display_task is not None:
-            self.display_task.cancel()
-        self.reset_tasks()
-        self.display_msg(msg)
-        
-    def set_display_delayed(self, msg: str, display_duration):        
-        if self.display_task is not None:
-            self.display_task.cancel()
-        self.display_msg(msg)
+
+    def _set_delay(self, display_duration: int):
         self.pending_task = asyncio.create_task(asyncio.sleep(display_duration))
-           
-    def reset(self):
-        self.clear_screen()
-        self.reset_tasks()
+
+    def _reset(self):
+        self._clear_screen()
+        self._reset_tasks()
         self.hold_display = False
         self.release_display = False
         self.press_display = False
         self.presses = 0
-        
+
+
 btn = InfoButton()
-
-
-        
